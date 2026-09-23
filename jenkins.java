832b8c97dd2001957641d6f8ce530c9817f9c5e6
Syeda Maham Batool <mahamabbas514@gmail.com>
2026-09-23 12:54:45 +0500

    Update jenkins.java

--- jenkins.java
+++ jenkins.java
@@ -1,6 +1,6 @@
 public class HelloJenkins {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!, version 2");
-        System.out.println("GitHub webhook successfully triggered Jenkins.");
+        System.out.println("GitHub webhook successfully triggered Jenkins.fatima is stupid");
     }
 }
