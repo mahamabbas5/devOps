289fadb9cae5bd312f940738fc2cf7f69a7bb4ce
Syeda Maham Batool <mahamabbas514@gmail.com>
2026-09-23 12:42:44 +0500

    Create jenkins.java

---
+++ jenkins.java
@@ -0,0 +1,6 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!, version 2");
+        System.out.println("GitHub webhook successfully triggered Jenkins.");
+    }
+}
